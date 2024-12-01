bba3e87ba579e004d19abe8397927ab1faadf9ca
cristian <cregueiromartinez@danielcastelao.org>
2024-12-01 12:36:45 +0100

    prefiero las composables xD

--- app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosFragment.kt
+++ app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosFragment.kt
@@ -7,9 +7,39 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.proyectopmdm.databinding.FragmentAudioBinding
+import com.example.proyectopmdm.databinding.FragmentDashboardBinding
 import com.example.proyectopmdm.ui.dashboard.DashboardViewModel
 
 class AudiosFragment : Fragment(){
 
+    private var _binding: FragmentAudioBinding? = null
 
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val audiosViewModel =
+            ViewModelProvider(this)[AudiosViewModel::class.java]
+
+        _binding = FragmentAudioBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textAudios
+        audiosViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
