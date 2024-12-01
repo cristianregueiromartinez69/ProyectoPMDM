546997649854ea81e486b20328b46199f8f51a2a
cristian <cregueiromartinez@danielcastelao.org>
2024-12-01 12:20:48 +0100

    icono añadido de audio

--- app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosFragment.kt
+++ app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosFragment.kt
@@ -1,6 +1,16 @@
 package com.example.proyectopmdm.ui.audios
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.proyectopmdm.ui.dashboard.DashboardViewModel
 
 class AudiosFragment : Fragment(){
+
+
+
 }
