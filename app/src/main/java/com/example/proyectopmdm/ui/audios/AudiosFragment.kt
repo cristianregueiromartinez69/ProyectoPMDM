a7d3cf60140977ccc90504b626be5f55ac242527
cristian <cregueiromartinez@danielcastelao.org>
2024-12-01 11:40:00 +0100

    empezando a hacer paerte de audios

---
+++ app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosFragment.kt
@@ -0,0 +1,6 @@
+package com.example.proyectopmdm.ui.audios
+
+import androidx.fragment.app.Fragment
+
+class AudiosFragment : Fragment(){
+}
---
+++ app/src/main/java/com/example/proyectopmdm/ui/audios/AudiosViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.proyectopmdm.ui.audios
+
+import androidx.lifecycle.ViewModel
+
+class AudiosViewModel : ViewModel(){
+}
