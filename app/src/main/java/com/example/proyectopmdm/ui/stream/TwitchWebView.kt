a19c7ec837b4c3c2d6e37cd00de68d40c7669f6b
Chema <jcaobaqueiro@danielcastelao.org>
2024-12-10 23:02:16 +0100

    modelo de vista para twitch

--- app/src/main/java/com/example/proyectopmdm/ui/stream/TwitchWebView.kt
+++ app/src/main/java/com/example/proyectopmdm/ui/stream/TwitchWebView.kt
@@ -1,4 +1,24 @@
 package com.example.proyectopmdm.ui.stream
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectopmdm.R
 
-class TwitchWebView {
+class TwitchWebView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitch_stream)
+
+        // Inicializamos el WebView
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // URL del stream de Twitch
+        val streamUrl = "https://www.twitch.tv/christmas24h"
+        //Cargamos la url del stream
+        webView.loadUrl(streamUrl)
+    }
 }
